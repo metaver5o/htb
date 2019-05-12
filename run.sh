c9b7a7069b9cc464e99a157b86e6d3b7bcc05f3e
Marco Matos <github@corp.mmatos.com>
2019-05-12 20:24:56 +0000

    adding run.sh

---
+++ run.sh
@@ -0,0 +1,1 @@
+docker run -tid --name htb --cap-add=NET_ADMIN --device=/dev/net/tun --memory 6g -e DISPLAY=:0 -v /home/marco/repo/htb:/repo  -v /tmp/.X11-unix:/tmp/.X11-unix -p 80:80 -p 443:443 m4rc0/private:htb bash
