a553e56dbbb7d522257ad112fe686bdfe80b2b8d
Marco Matos <github@corp.mmatos.com>
2019-05-12 20:27:17 +0000

    fising run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-docker run -tid --name htb --cap-add=NET_ADMIN --device=/dev/net/tun --memory 6g -e DISPLAY=:0 -v /home/marco/repo/htb:/repo  -v /tmp/.X11-unix:/tmp/.X11-unix -p 80:80 -p 443:443 m4rc0/private:htb bash
+docker run -tid --name htb --cap-add=NET_ADMIN --device=/dev/net/tun --memory 6g -e DISPLAY=:0 -v /home/marco/repo/htb:/repo  -v /tmp/.X11-unix:/tmp/.X11-unix m4rc0/private:htb bash
+
+# ports if needed
+#  -p 80:80 -p 443:443 
