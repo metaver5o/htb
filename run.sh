040b1628545b67433d344b63e27565e4f115f3c2
Marco Matos <marco_aurelio_matos@epam.com>
2019-06-26 12:09:37 +0200

    creating osx branch

--- run.sh
+++ run.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 image=htb ;
+docker stop ${image} || true && docker rm ${image} || true
+
 docker build -t ${image} . ;
 docker run -it \
 	--cap-add=SYS_ADMIN \
 	--cap-add=NET_ADMIN \
 	--cpuset-cpus 0 \
 	--memory 4GB \
-	-v /tmp/.X11-unix:/tmp/.X11-unix \
-	-e DISPLAY=unix:0 \
-	--device /dev/snd \
-	--device /dev/dri \
-	-v /dev/shm:/dev/shm  \
+	-e DISPLAY=host.docker.internal:0 \
 	--name htb  \
-	--device=/dev/net/tun \
-	-v /home/marco/repo/htb:/repo \
-	--ip6 a:b:c:d::1234 \
-	--net=mynetwork \
+	-v /Users/marco_aurelio_matos/repo/htb:/repo \
 	${image} bash;
 
-docker attach htb
+#docker attach htb
